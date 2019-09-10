86d8640d96b48ea6251391cd04e0645117fb3236
Michail <zamotin-mihail@mail.ru>
2019-09-10 15:26:14 +0300

    testing with unittest

--- selenium_env/test_abs_project.py
+++ selenium_env/test_abs_project.py
@@ -8,6 +8,5 @@ class TestAbs(unittest.TestCase):
     def test_abs2(self):
         self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
 
-
 if __name__ == "__main__":
     unittest.main()

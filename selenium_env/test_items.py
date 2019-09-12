97af49fee39079c1e20adffc73b37694f1e97e93
Michail <zamotin-mihail@mail.ru>
2019-09-12 23:42:11 +0300

    добавил файл test_items

--- selenium_env/test_items.py
+++ selenium_env/test_items.py
@@ -1,5 +1,7 @@
+import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_should_be_basket(browser):
+def test_should_be_add_to_basket_button(browser):
     browser.get(link)
+    time.sleep(20)
     assert browser.find_element_by_css_selector(".btn-add-to-basket"), "No add to basket button"

ca545fc8c2b61c43337b14cf3a8cfbbc2d611169
Snoopke <zamotin-mihail@mail.ru>
2019-09-18 17:09:47 +0300

    задание надо доделать

--- stepik/rost_v_klasse.py
+++ stepik/rost_v_klasse.py
@@ -5,5 +5,5 @@ ans = []
 with open("dataset_3380_5.txt") as classmates:
     reader = csv.reader(classmates, delimiter='\t')
     for row in reader:
-        print(" ".join(row))
+        print(row)
 

1115e5a7760fc96f40cd325c11e473f15aaf88a7
Snoopke <zamotin-mihail@mail.ru>
2020-02-09 15:30:03 +0300

    add 'picture'

--- game_hangman.py
+++ game_hangman.py
@@ -82,22 +82,7 @@ def get_available_letters(lettersGuessed):
 def hangman(secret_word):
     """
     secretWord: string, the secret word to guess.
-
     Starts up an interactive game of Hangman.
-
-    * At the start of the game, let the user know how many
-      letters the secretWord contains.
-
-    * Ask the user to supply one guess (i.e. letter) per round.
-
-    * The user should receive feedback immediately after each guess
-      about whether their guess appears in the computers word.
-
-    * After each round, you should also display to the user the
-      partially guessed word so far, as well as letters that the
-      user has not yet guessed.
-
-    Follows the other limitations detailed in the problem write-up.
     """
     guesses_left = 8
     lettersGuessed = []
@@ -122,6 +107,12 @@ def hangman(secret_word):
                 if guesses_left == 0:
                     print('-----------')
                     print('Sorry, you ran out of guesses. The word was {}.'.format(secretWord))
+                    print(' #########')
+                    print('  |     ##')
+                    print('  O     ##')
+                    print(' /|\    ##')
+                    print(' / \    ##')
+                    print('________##')
                     break
                 else:
                     continue
